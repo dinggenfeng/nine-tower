a46340c27f93b32729dcc03449973ad8c2b8bd56
dinggenfeng <dinggenfeng@gmail.com>
2026-04-13 16:16:59 +0800

    feat(frontend): add centralized Ant Design theme configuration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,19 @@
-import { App as AntApp } from "antd";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "antd/dist/reset.css";
+import { App as AntApp, ConfigProvider } from 'antd';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { theme } from './theme';
+import 'antd/dist/reset.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AntApp>
-        <App />
-      </AntApp>
+      <ConfigProvider theme={theme}>
+        <AntApp>
+          <App />
+        </AntApp>
+      </ConfigProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
