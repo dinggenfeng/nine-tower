ede5567f61ba05d0960fddf8e9019f5911607ae7
dinggenfeng <dinggenfeng@gmail.com>
2026-04-16 18:05:26 +0800

    feat: add TemplateRepository

---
+++ backend/src/main/java/com/ansible/role/repository/TemplateRepository.java
@@ -0,0 +1,12 @@
+package com.ansible.role.repository;
+
+import com.ansible.role.entity.Template;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemplateRepository extends JpaRepository<Template, Long> {
+
+  List<Template> findAllByRoleIdOrderByParentDirAscNameAsc(Long roleId);
+
+  boolean existsByRoleIdAndParentDirAndName(Long roleId, String parentDir, String name);
+}
